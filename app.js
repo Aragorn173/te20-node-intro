07f93b04d5a0e025ab65768a3996308b397cb460
Aragorn173 <alvin.liljestrom@elev.ga.ntig.se>
2023-01-17 14:48:36 +0100

    lade till sass och innehåll på sidan och hamburgarmeny

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path')
 const app = express();
 const port = 3000;
 const indexRouter = require('./routes/index');
+const createError = require('http-errors')
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
@@ -17,6 +18,20 @@ nunjucks.configure('views', {
     express: app,
 });
 
-
 app.use(express.static('public'))
-app.use('/static', express.static(path.join(__dirname, 'public')))
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error.njk');
+});
